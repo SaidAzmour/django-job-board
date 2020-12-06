19032622c5e8f5ba201137fb9ae10c4fa00c9ba7
SaidAzmour <66140703+SaidAzmour@users.noreply.github.com>
2020-12-06 22:52:38 +0100

    first commit

---
+++ job/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+
+JOB_TYPE = {
+    ('Full Time','Full Time'),
+    ('Part Time','Part Time'),
+}
+class Job(models.Model):
+    title = models.CharField(max_length=50)  #column
+    #location
+    job_type = models.CharField(max_length=15 , choices=JOB_TYPE)
+    description = models.TextField(max_length=1000)
+    peblished_at = models.DateTimeField(auto_now=True)
+    Vacancy = models.IntegerField(default=1)
+    Salary = models.IntegerField(default=0)
+    Experience = models.IntegerField(default=1)
+    category = models.ForeignKey('Category',on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
